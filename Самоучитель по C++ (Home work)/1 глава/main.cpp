9f81767f0a3a48cc6d2db42edffcb34ab8e139f6
kogami-shinya <109354766+kogami-shinya@users.noreply.github.com>
2022-10-25 14:57:40 +0300

    Add files via upload

---
+++ Самоучитель по C++ (Home work)/1 глава/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "io.h"
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int x, y;
+    x = readNumber();
+    y = readNumber();
+    writeAnswer(x + y);
+
+    cin.clear();
+    cin.ignore(32767, '\n');
+    cin.get();
+    return 0;
+}
